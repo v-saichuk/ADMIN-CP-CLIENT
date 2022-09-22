59572fcd792e119b90b67b3f7bc62fe05c4fd181
Admin <admin@MacBook-Pro-Admin.local>
2022-09-22 19:15:13 +0300

    Update files, Skeleton, MongoDB, Language

--- src/views/Authorized/Settings/SettingsForm/Language.settings/LanguageSettings.tsx
+++ src/views/Authorized/Settings/SettingsForm/Language.settings/LanguageSettings.tsx
@@ -1,18 +1,19 @@
 import { FC, useEffect } from 'react';
-import { Button, Col, List, Row, Skeleton, Switch } from 'antd';
+import { Col, List, Row, Switch, Skeleton } from 'antd';
 import { useAppDispatch, useAppSelector } from '../../../../../store/hooks/useRedux';
 import { handleLanguage } from '../../../../../store/settings/language.slice';
-// import { getCountry } from '../../../../../store/settings/language.slice';
+import { getCountry } from '../../../../../store/settings/language.slice';
+import { FlagIcon } from '../../../../../components/FlagIcon/FlagIcon';
 
 import './LanguageSettings.scss';
 
 export const LanguageSettings: FC = () => {
-    const { lang } = useAppSelector((state) => state.language);
+    const { lang, isLoading } = useAppSelector((state) => state.language);
     const dispatch = useAppDispatch();
 
-    // useEffect(() => {
-    //     dispatch(getCountry());
-    // }, [dispatch]);
+    useEffect(() => {
+        dispatch(getCountry());
+    }, [dispatch]);
 
     return (
         <Row gutter={[16, 24]}>
@@ -36,19 +37,38 @@ export const LanguageSettings: FC = () => {
                     renderItem={(item) => (
                         <List.Item style={{ marginBottom: '10px' }}>
                             <div className="language__item">
-                                <div className="language__box">
-                                    <div className="language__flag">{item.icon}</div>
-                                    <div className="language__country">{item.title}</div>
-                                </div>
-                                <div className="language__swich">
-                                    <Switch
-                                        size="small"
-                                        defaultChecked={item.enabled}
-                                        onChange={(checked: boolean) =>
-                                            dispatch(handleLanguage([checked, item.code]))
-                                        }
-                                    />
-                                </div>
+                                {isLoading ? (
+                                    // TODO: Зробити автоматичне додавання кількості блоків щоб не було скачків перед першим рендером
+                                    <div className="language__skeleton">
+                                        <div style={{ marginRight: 10 }}>
+                                            <Skeleton.Avatar size={'small'} shape={'square'} />
+                                        </div>
+                                        <div style={{ width: '100%' }}>
+                                            <Skeleton.Input
+                                                size="small"
+                                                className="language__skeleton-box-wid"
+                                            />
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <>
+                                        <div className="language__box">
+                                            <div className="language__flag">
+                                                <FlagIcon code={item.code} />
+                                            </div>
+                                            <div className="language__country">{item.title}</div>
+                                        </div>
+                                        <div className="language__swich">
+                                            <Switch
+                                                size="small"
+                                                defaultChecked={item.enabled}
+                                                onChange={(checked: boolean) =>
+                                                    dispatch(handleLanguage([checked, item.code]))
+                                                }
+                                            />
+                                        </div>
+                                    </>
+                                )}
                             </div>
                         </List.Item>
                     )}
